9c3aa2cf69cc22a2ed65148ad59573b240c3f1b2
Erlend <88742199+erlwes@users.noreply.github.com>
2024-10-03 22:49:21 +0200

    Update and rename WinRM-Cloak-Installer.ps1 to WinRM-Cloak.ps1
    1. Cleaned up after extracting the WinRM-hardening to separate script.
    2. Added some error handling to SC CREATE

---
+++ WinRM-Cloak.ps1
@@ -0,0 +1,223 @@
+<#
+  .SYNOPSIS
+    A script for cloaking PowerShell remoting (WinRM-service), by hiding the service when not explicitly unlocked/started by sending a secret string to a listener.
+
+  .DESCRIPTION
+    The script install a gatekeeper/cloak-service that will start WinRM-service only when a secret string is sent to listener
+        - Deploys a PowerShell UDP-listener as a service
+        - This service will listen for data on a specified UDP port (no reply back to sender)
+        - If correct string is received -> Start WinRM-service, Sleep for x amount of seconds and stop WinRM-service again
+        - This means that you have to do a secret knock on door A, for door B to open
+
+    Thoughts/general considerations
+    1. When selecting a port. Avoid commons ports. One way of doing this would be to pick a port
+       that is not on nmap top port list (nmap --top-ports 20000 localhost -v -oG -).
+    2. Where ever you decide to drop the service script on disk, make sure that ...
+        A) Folder/file-ACL does not allow the script to be edited by non-administrators (service hijacking). Running as system by default.
+        B) Folder/file-ACL does not allow the script to be read by non-administrators, Secret string is hardcoded in listener script for now.
+#>
+
+#Requires -RunAsAdministrator
+
+Param (
+    [Parameter(Mandatory = $false, ParameterSetName = 'Install')]
+    [int]$CloakPort,
+
+    [Parameter(Mandatory = $false, ParameterSetName = 'Remove')]
+    [switch]$Remove,
+
+    [Parameter(Mandatory = $false, ParameterSetName = 'Monitor')]
+    [switch]$Monitor
+)
+
+# [FUNCTIONS]
+Function Write-Log {
+    param(
+        [ValidateSet(0, 1, 2, 3, 4)]
+        [int]$Level,
+
+        [Parameter(Mandatory=$true)]
+        [string]$Message            
+    )
+    $Message = $Message.Replace("`r",'').Replace("`n",' ')
+    switch ($Level) {
+        0 { $Status = 'Info'    ;$FGColor = 'White'   }
+        1 { $Status = 'Success' ;$FGColor = 'Green'   }
+        2 { $Status = 'Warning' ;$FGColor = 'Yellow'  }
+        3 { $Status = 'Error'   ;$FGColor = 'Red'     }
+        4 { $Status = 'Console' ;$FGColor = 'Gray'    }
+        Default { $Status = ''  ;$FGColor = 'Black'   }
+    }
+    
+    Write-Host "$((Get-Date).ToString()) " -ForegroundColor 'DarkGray' -NoNewline
+    Write-Host "$Status" -ForegroundColor $FGColor -NoNewline
+
+    if ($level -eq 4) {
+        Write-Host ("`t " + $Message) -ForegroundColor 'Cyan'
+    }
+    else {
+        Write-Host ("`t " + $Message) -ForegroundColor 'White'
+    }
+    
+    if ($Level -eq 3) {
+        $LogErrors += $Message
+    }
+}
+
+switch ($PSCmdlet.ParameterSetName) {
+    'Remove'  {        
+        Write-Host "[REMOVING SERVICE]" -ForegroundColor Cyan
+        if (Get-Service WinRM-Cloak -ErrorAction SilentlyContinue) {
+            Write-Log -Level 0 -Message "Get-Service - Service 'WinRM-Cloak' exists"
+            try {
+                Stop-Service WinRM-Cloak -Force -ErrorAction Stop
+                Write-Log -Level 1 -Message "Stop-Service - Service 'WinRM-Cloak' stopped"
+            }
+            catch {
+                Write-Log -Level 3 -Message "Stop-Service - Failed to stop service 'WinRM-Cloak'. Error: $($_.Exception.Message)"
+            }
+            
+            SC.EXE DELETE WinRM-Cloak
+            if ($LASTEXITCODE -eq 0) {
+                Write-Log -Level 1 -Message "WinRM CLOAK - Service deleted (Exitcode: $LASTEXITCODE)"
+            }
+            else {
+                Write-Log -Level 3 -Message "WinRM CLOAK - Failed to delete service (Exitcode: $LASTEXITCODE)"
+            }              
+            Break
+        }
+        else {
+            Write-Log -Level 2 -Message "Get-Service - Service 'WinRM-Cloak' does not exist and can therefore not be removed."
+            Break
+        }
+    }
+    'Monitor' {
+        Function Invoke-WinRMMonitor {            
+            while ($true) {
+                Clear-Host
+                Clear-Variable Port -ErrorAction SilentlyContinue    
+                $Port = (Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WSMAN\Listener\*+HTTP\').Port
+                '';Write-Host "[MONITORING]" -ForegroundColor Cyan
+                if((Get-Service WinRM).Status -eq 'Stopped') {
+                    Write-Host "$((Get-Date).ToString()) " -ForegroundColor DarkGray -NoNewline
+                    Write-Host "WinRM is stopped (TCP $Port)" -ForegroundColor Yellow
+                    
+                }
+                elseif((Get-Service WinRM).Status -eq 'Running') {
+                    if (Get-PSSession -ComputerName localhost -Port $Port -ErrorAction SilentlyContinue) {
+                        Write-Host "$((Get-Date).ToString()) " -ForegroundColor DarkGray -NoNewline
+                        Write-Host "WinRM is running (TCP $Port), and connected" -ForegroundColor Cyan
+                    }
+                    else {
+                        Write-Host "$((Get-Date).ToString()) " -ForegroundColor DarkGray -NoNewline
+                        Write-Host "WinRM is running (TCP $Port)" -ForegroundColor Green                        
+                    }        
+                }
+
+                Clear-Variable CloakService -ErrorAction SilentlyContinue
+                $CloakService = Get-CimInstance Win32_Service -Filter "Name = 'WinRM-Cloak'"
+                                
+                if($CloakService.State -eq 'Stopped') {
+                    Write-Host "$((Get-Date).ToString()) " -ForegroundColor DarkGray -NoNewline
+                    Write-Host "WinRM-Cloak is stopped ($Port)" -ForegroundColor Yellow
+                }
+                elseif($CloakService.State -eq 'Running') {
+
+                    Clear-Variable ServiceChildProcess, UDPEndpoint -ErrorAction SilentlyContinue
+                    $ServiceChildProcess = Get-CimInstance Win32_Process | Where-Object {$_.ParentProcessId -eq $CloakService.ProcessId}
+                    $UDPEndpoint = Get-NetUDPEndpoint -OwningProcess $ServiceChildProcess.ProcessId
+
+                    Write-Host "$((Get-Date).ToString()) " -ForegroundColor DarkGray -NoNewline
+                    Write-Host "WinRM-Cloak is running (UDP $($UDPEndpoint.LocalPort))" -ForegroundColor Green
+                }
+                Start-Sleep -s 2
+            }
+        }
+        Invoke-WinRMMonitor;Break
+    }
+    'Install' {
+        #Region WinRM
+        Write-Host "[INSTALLING SERVICE]" -ForegroundColor Cyan
+        # [WINRM - SET SERVICE STARTUP TO MANUAL]
+        try {
+            if ((Get-Service WinRM).StartType -ne 'Manual') {
+                Set-Service WinRM -StartupType Manual -ErrorAction Stop    
+                Write-Log -Level 1 -Message "WINRM - Set service startuptype to 'Manual'"
+            }
+            else {
+                Write-Log -Level 0 -Message "WINRM - Service startuptype already set to 'Manual'"
+            }
+        }
+        catch {
+            Write-Log -Level 3 -Message "WINRM - Failed to set service startuptype to 'Manual'. Error: $($_.Exception.Message)"
+        }
+
+        # [WINRM - STOP SERVICE]
+        try {
+            if ((Get-Service WinRM).Status -eq 'Running') {
+                Stop-Service WinRM -Force -ErrorAction Stop
+                Write-Log -Level 1 -Message 'WINRM - Service stopped'
+            }
+        }
+        catch {
+            Write-Log -Level 3 -Message "WINRM - Failed to stop service. Error: $($_.Exception.Message)"
+        }
+        #Endregion WinRM
+
+        #Region Firewall
+        # OPEN UDP LISTERNER/DOORKEEPER PORT
+        $FirewallParam2 = @{
+            DisplayName = 'WinRM - Doorkeeper service'
+            Description = "Allow Windows to listen on UDP port $CloakPort"
+            Direction = 'Inbound'
+            LocalPort = $CloakPort
+            Protocol = 'UDP'
+            Action = 'Allow'
+            Profile = 'Private', 'Domain'
+            Program = 'Any' #PowerShell.exe for manually testing WinRM-Cloak-Service.ps1, and SYSTEM/Srvstart.exe for service. Could limit further.
+        }
+        try {
+            New-NetFirewallRule @FirewallParam2 -ErrorAction Stop -Confirm:$false | Out-Null
+            Write-Log -Level 1 -Message "FIREWALL - New rule '$($FirewallParam2.DisplayName)' created (UDP $CloakPort inbound)"
+        }
+        catch {
+            Write-Log -Level 3 -Message "FIREWALL - Failed to create new rule '$($FirewallParam2.DisplayName)'. Error: $($_.Exception.Message)"
+        }
+        #Endregion Firewall
+
+
+        #Region Cloak service
+        # [WinRM-Cloak - CREATE SERVICE FOR UDP LISTENER]
+        if (Test-Path -Path "$PSScriptRoot\WinRM-Cloak-Service.ini") {
+            Remove-Item "$PSScriptRoot\WinRM-Cloak-Service.ini" -Force -Confirm:$false
+        }
+        try {
+            $ServiceConfig = @()
+            $ServiceConfig += "[WinRM-Cloak]`n"
+            $ServiceConfig += "startup=C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Unrestricted -NoProfile -File $PSScriptRoot\WinRM-Cloak-Service.ps1 $CloakPort`n"
+            $ServiceConfig += "shutdown_method=kill"
+            $ServiceConfig | Out-File "$PSScriptRoot\WinRM-Cloak-Service.ini" -Force -Encoding ASCII -NoNewline -ErrorAction Stop
+            Write-Log -Level 1 -Message "WinRM CLOAK - Created service config-file ('$PSScriptRoot\WinRM-Cloak-Service.ini')"
+            # If you edit the .ini file manually, make sure it is saved as ANSI after changes. The service will not start if UTF8.
+        }
+        catch {
+            Write-Log -Level 3 -Message "WinRM CLOAK - Failed to create service config-file ('$PSScriptRoot\WinRM-Cloak-Service.ini'). Error: $($_.Exception.Message)"
+        }
+
+
+        SC.EXE CREATE WinRM-Cloak Displayname= "WinRM-Cloak" binpath= "$PSScriptRoot\srvstart.exe WinRM-Cloak -c $PSScriptRoot\WinRM-Cloak-Service.ini" start=auto | Out-Null
+        if ($LASTEXITCODE -eq 0) {
+            Write-Log -Level 1 -Message "WinRM CLOAK - Service created (Exitcode: $LASTEXITCODE)"
+        }
+        elseif ($LASTEXITCODE -eq 1073) {
+            Write-Log -Level 2 -Message "WinRM CLOAK - Service 'WinRM-Cloak' already exist (Exitcode: $LASTEXITCODE)"
+        }
+        elseif ($LASTEXITCODE -eq 1072) {
+            Write-Log -Level 3 -Message "WinRM CLOAK - Service 'WinRM-Cloak' is marked for deletion (Exitcode: $LASTEXITCODE)"
+        }
+        else {
+            Write-Log -Level 3 -Message "WinRM CLOAK - Failed to create service (Exitcode: $LASTEXITCODE)"
+        }
+        #Endregion Cloak service
+    }
+}
